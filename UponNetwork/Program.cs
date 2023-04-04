6db8477bf5db4997d201cf0d959fb6b5e716c9ae
ASMcoder <vitaliynovak555@gmail.com>
2023-04-04 15:23:38 +0300

    JUST TEST COMMIT FOR DEPLOY TEST

--- UponNetwork/Program.cs
+++ UponNetwork/Program.cs
@@ -20,7 +20,7 @@ class Program
 
     static async Task Main(string[] args)
     {
-        Console.WriteLine("Testing deploy config!");
+        Console.WriteLine("Testing deploy config!, this shit broken?");
         node = CreateNewNode(1300, "user");
         node.NodeReceivedTechnicalMessage += AnyMessageHandler;
         node.NodeReceivedMessage += AnyMessageHandler;
